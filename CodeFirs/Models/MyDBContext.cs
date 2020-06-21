b951114015258235a76fce11d10c1ecc4396fea4
Michał <michal_duda@onet.pl>
2020-06-21 22:57:26 +0200

    Dodanie DBcontext +ustawienie danych do logowania

---
+++ CodeFirs/Models/MyDBContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirs.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public DbSet<Medicament> Medicament { get; set; }
+        public DbSet<Prescription> Prescription { get; set; }
+        public DbSet<Prescription_Medicament> Prescription_Medicament { get; set; }
+        public DbSet<Doctor> Doctor { get; set; }
+        public DbSet<Patient> Patient { get; set; }
+
+        public MyDBContext() { }
+
+        public MyDBContext(DbContextOptions options)
+        : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Prescription_Medicament>()
+                         .HasKey(e => new { e.IdMedicament, e.IdPrescription });
+
+        }
+    }
+}
