500c25fde4f6c23092da8e61a369e8468a2a8bfc
Michał <michal_duda@onet.pl>
2020-06-21 22:46:37 +0200

    dodanie klas opisujacych model danych

---
+++ CodeFirs/Models/Prescription.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirs.Models
+{
+    public class Prescription
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdPrescription { get; set; }
+
+        
+        public DateTime Date { get; set; }
+
+
+        public DateTime DueDate { get; set; }
+
+        [ForeignKey("Doctor")]
+        public int IdDoctor { get; set; }
+        public Doctor Doctor { get; set; }
+
+        [ForeignKey("Patient")]
+        public int IdPatient { get; set; }
+        public Patient Patient { get; set; }
+
+        public ICollection<Prescription_Medicament> Prescription_Medicament { get; set; }
+    }
+}
---
+++ CodeFirs/Models/Prescription_Medicament.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirs.Models
+{
+    public class Prescription_Medicament
+    {
+        public int Dose { get; set; }
+
+        [MaxLength(100)]
+        public string Details { get; set; }
+
+        [ForeignKey("Medicament")]
+        public int IdMedicament { get; set; }
+        public Medicament Medicament { get; set; }
+
+        [ForeignKey("Prescription")]
+        public int IdPrescription { get; set; }
+        public Prescription Prescription { get; set; }
+
+
+    }
+}
