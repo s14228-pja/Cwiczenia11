1b026fd585b88e1cc9a093c507cdbdfd6ee19bc2
Michał <michal_duda@onet.pl>
2020-06-21 23:13:32 +0200

    dodanie koncowki getDoctors

---
+++ CodeFirs/Controllers/DoctorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeFirs.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeFirs.Controllers
+{
+    [Route("api/doctor")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        public MyDBContext _myDBContext;
+
+        public DoctorController (MyDBContext myDBContext)
+        {
+            _myDBContext = myDBContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetDoctor()
+        {
+            return Ok(_myDBContext.Doctor.ToList());
+        }
+
+        [HttpPut]
+        public IActionResult UpdateDoctor()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddDoctor()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteDoctor()
+        {
+            return Ok();
+        }
+    }
+}
